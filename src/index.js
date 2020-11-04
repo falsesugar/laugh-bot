12b5574ef50be2b40b4957b7809e0733acb7a836
falsesugar <50983382+falsesugar@users.noreply.github.com>
2020-11-04 00:27:07 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,85 @@
+import "./styles.css";
+
+let face1 = document.getElementById("face1");
+let faceImg = document.getElementById("face");
+
+let laughs = [
+  "haha, that is so funny",
+  "wow. hilarious",
+  "L O L",
+  "I swear I am laughing inside",
+  "L M F A O"
+];
+
+let laughCount = 0;
+
+function enableAutoTTS() {
+  if (typeof window === "undefined") {
+    return;
+  }
+  const isiOS =
+    navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+  if (!isiOS) {
+    return;
+  }
+  const simulateSpeech = () => {
+    const lecture = new SpeechSynthesisUtterance("hello");
+    lecture.volume = 0;
+    speechSynthesis.speak(lecture);
+    document.removeEventListener("click", simulateSpeech);
+  };
+
+  document.addEventListener("click", simulateSpeech);
+}
+
+enableAutoTTS();
+
+console.log("hello from script");
+const synth = window.speechSynthesis;
+
+const speak = (text) => {
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+  let utterThis = new SpeechSynthesisUtterance(text);
+  synth.speak(utterThis);
+};
+
+function speakFunction() {
+  speak(laughs[laughCount]);
+  console.log("speaking");
+}
+
+function laughSelected() {
+  laughCount = (laughCount + 1) % laughs.length;
+  speakFunction();
+}
+
+let notSmile = true;
+
+function switchFace() {
+  if (notSmile) {
+    faceImg.src = "./img/face1.svg";
+  } else {
+    faceImg.src = "./img/face2.svg";
+  }
+}
+
+face1.addEventListener("click", laughSelected);
+
+face1.addEventListener("mousedown", mouseDown);
+
+face1.addEventListener("mouseup", mouseUp);
+
+function mouseDown() {
+  notSmile = false;
+  switchFace();
+  console.log("mouse down!");
+}
+
+function mouseUp() {
+  notSmile = true;
+  switchFace();
+  console.log("mouse up!");
+}
